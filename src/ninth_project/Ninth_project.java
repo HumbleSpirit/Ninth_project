7a5687a5d4a6bb90329502af1a59b93c9a2574af
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-10 18:36:15 +0300

    Неизменяемые строки

---
+++ src/ninth_project/Ninth_project.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ninth_project;
+
+/**
+ *
+ * @author Boro_s99
+ */
+public class Ninth_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*String str1 = "Vasya";
+        String str2;
+        str2 = str1; //Строки не изменяемые
+        System.out.println(str1);
+        System.out.println(str2);
+        str2 = "Kolya";
+        System.out.println(str1);
+        System.out.println(str2);*/
+        
+        
+    }
+    
+}
